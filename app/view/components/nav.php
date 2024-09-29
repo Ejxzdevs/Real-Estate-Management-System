fb8d90e114f7709bd7cc0734534077dd9e22045a
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-09-30 05:48:26 +0800

    modified

--- app/view/components/nav.php
+++ app/view/components/nav.php
@@ -1,5 +1,31 @@
-<nav>
-    <a href="?route=home">HOME</a>
-    <a href="?route=product">PRODUCT</a>
-    <a href="?route=a">tae</a>
-</nav>
+<div class="navContainer d-flex flex-row" style="height: 4rem; width: 100%; top: 0; position: fixed;" >
+    <figure class="h-100 d-lg-flex d-none justify-content-center align-items-center w-25 w-lg-25">
+        <figcaption class="text-white">EjHayyy</figcaption>
+    </figure>
+    <nav class="navbar navbar-expand-lg w-100 w-lg-50">
+        <button class="ms-3 navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navExpandContainer collapse navbar-collapse" id="navbarNav">
+            <ul class="w-100 ps-3 pt-3 pb-3 navbar-nav gap-2 gap-lg-3 d-flex flex-column flex-lg-row justify-content-center align-items-start justify-content-lg-center align-items-lg-center"> <!-- Added d-flex and gap-5 -->
+                <li class="nav-item">
+                    <a class="nav-link text-light active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light " href="#">Rent</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light " href="#">Buy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light " href="#" tabindex="-1" aria-disabled="true">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="h-100 flex-row d-lg-flex d-none justify-content-center align-items-center gap-4 w-25 w-lg-25">
+        <a class="text-light text-decoration-none" href="#">Login</a>
+        <button class="btn btn-light">Get Started</button>
+    </div>
+</div>
