b427ddd41a886ce155425ccb29284f4c1cebfcd9
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-09-30 02:44:33 +0800

    add google font

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHML</title>
-    <link rel="stylesheet" href="public/assets/style.global.css">
+    <link rel="stylesheet" href="public/assets/style/global.css">
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 </head>
 <body>
