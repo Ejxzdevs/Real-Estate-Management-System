72122ea0951196db1aa67cce27ec047448f5f447
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-10-07 23:34:29 +0800

    change the title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHML</title>
+    <title>Real Estate Management System</title>
     <link rel="stylesheet" href="public/assets/styles/global.css">
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 </head>
@@ -20,6 +20,8 @@
     <footer>
         <?php require_once "app/view/components/footer.php"; ?>
     </footer>
+
+
      <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
