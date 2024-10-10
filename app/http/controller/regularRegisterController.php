87ae5ce68925701498138e58503c7c38bd10a005
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-10-11 05:23:46 +0800

    added backend for register

--- app/http/controller/regularRegisterController.php
+++ app/http/controller/regularRegisterController.php
@@ -5,10 +5,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = htmlspecialchars($_POST['username'] ?? '', ENT_QUOTES, 'UTF-8');
     $password = htmlspecialchars($_POST['password'] ?? '', ENT_QUOTES, 'UTF-8');
     $repassword = htmlspecialchars($_POST['rePassword'] ?? '', ENT_QUOTES, 'UTF-8');
-
     if($password !== $repassword){
         echo "<script>alert('password did not match')</script>";
     }else{
-        echo "match";
+        if(!empty($username) && !empty($password)){
+            try {
+                $createUser = new UserRegister();
+                $createUser->insertUser($username, $password);
+                echo "<script>alert('User registered successfully!')</script>";
+                echo "<script> window.location.href='../../../index.php'</script>";
+            } catch (Exception $e) {
+                echo "<script>alert('Error: " . $e->getMessage() . "')</script>";
+            }
+        }else{
+            echo "password or username is empty";
+        }
+        
     }
 }
--- app/model/regularRegister.php
+++ app/model/regularRegister.php
@@ -1,5 +1,31 @@
 <?php 
+require_once '../../../config/mysql.php';
+class UserRegister extends MYSQL {
+    private $username;
+    private $password;
+    private $hashed_password;
+    private $userType = "regular";
+    public function insertUser($username,$password){
+        parent::__construct();
+        $this->username = $username;
+        $this->password = $password;
+        try {
+            $this->openMySqlConnection();
+            $this->hashed_password = password_hash($this->password, PASSWORD_DEFAULT);
+            $insert_account = "INSERT INTO user_account (username, password, user_type) VALUES (:name, :hashed_password, :type)";
+            $stmt = $this->conn->prepare($insert_account);
+            $stmt->bindParam(':name', $this->username);
+            $stmt->bindParam(':hashed_password', $this->hashed_password);
+            $stmt->bindParam(':type', $this->userType);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        } finally {
+            $this->closeMySqlConnection();
+        }
 
-class RegularRegister {
-    
+    }
+
+
+
 }
