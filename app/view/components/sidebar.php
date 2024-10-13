2071cc1c4dc61a3b033a5463cd67e8ac52667039
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-10-13 19:39:36 +0800

    added icons and style

--- app/view/components/sidebar.php
+++ app/view/components/sidebar.php
@@ -3,12 +3,23 @@
         <i class="bi bi-houses"></i><span>REAL STATE</span>
     </div>
     <div class="menu">
+        
         <ul>
+            <h5>Main Menu</h5>
             <li>
-                <i class="bi bi-ui-checks-grid"></i><a href="?route=dashboard">home</a>
+                <i class="bi bi-ui-checks-grid"></i><a href="?route=dashboard">Dashboard</a>
             </li>
             <li>
-                <i class="bi bi-house"></i><a href="?route=product">product</a>
+                <i class="bi bi-house"></i><a href="?route=product">Property</a>
+            </li>
+            <li>
+                <i class="bi bi-question-circle"></i><a href="?route=product">Inquiries</a>
+            </li>
+            <li>
+                <i class="bi bi-file-earmark-text"></i>Reports</a>
+            </li>
+            <li>
+                <i class="bi bi-gear"></i><a href="?route=product">Settings</a>
             </li>  
             <li>
                 <i class="bi bi-box-arrow-in-left"></i><a href="app/http/controller/logout.php">Logout</a>
