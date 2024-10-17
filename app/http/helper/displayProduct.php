46b19087781ea426944abc9cf6117eb6f01f388c
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-10-17 12:11:24 +0800

    added new listing section

--- app/http/helper/displayProduct.php
+++ app/http/helper/displayProduct.php
@@ -1,10 +1,25 @@
 <?php 
 require_once 'config/mysql.php';
+
 class DisplayProduct extends MySQL {
 
     public function getAllProducts() {
         try {
             $stmt = parent::openConnection()->prepare("SELECT * FROM properties");
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        } finally {
+            parent::closeConnection();
+        }
+    }
+
+    public function recentProducts($limit) {
+        try {
+            $stmt = parent::openConnection()->prepare("SELECT * FROM properties ORDER BY date_added DESC LIMIT :limit");
+            $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
--- app/view/home.php
+++ app/view/home.php
@@ -1,3 +1,8 @@
+<?php 
+    include "app/http/helper/displayProduct.php";
+    $fetchproducts = new DisplayProduct();
+    $products = $fetchproducts->recentProducts(6);
+?>
 <div class="homeContainer">
     <div class="introContainer row ">
         <div class="col-md-6 left">
@@ -15,36 +20,32 @@
             </p>
         </div>
     </div>
-    <div class="popoularHouseContainer">
-        <h1>Popular Listing</h1>
+    <div class="container-fluid">
+        <h1 class="text-center display-6" >New Listing</h1>
+        <div class="row d-flex justify-content-center align-items-center gap-5 bg-secondary p-4">
+            <!-- First Item -->
+            <?php foreach($products as $product): ?>
+                <div class="card border d-flex flex-row p-0 " style="height: 200px; width: 550px; cursor: pointer; " >
+                    <div class="border p-1" style="width: 50%;" >
+                        <img class="h-100 rounded-3" src="public/images/products/<?php echo $product['image']; ?>" alt="">
+                    </div>
+                    <div class="d-flex flex-column ps-2" style="width: 60%;" >
+                        <div class="d-flex flex-column m-0 p-0" style="height: auto;" >
+                            <p class="m-0 fw-bold fs-6"><?php echo $product['name']; ?></p>
+                            <p class="m-0 text-secondary" style="font-size: 10px;" ><i class="bi bi-geo-alt"></i><?php echo $product['address']; ?></p>
+                        </div>
+                        <div class="ps-1 pe-2  py-0" style="height: 59%;">
+                            <p class="mt-4 mx-0 text-wrap" style="font-size: 10px;" ><?php echo $product['description']; ?></p>
+                        </div>
+                        <div class="ps-1 pe-4 d-flex flex-row justify-content-between align-items-center" style="height: 18%;" >
+                            <p class="m-0 text-secondary" >₱<?php echo $product['price']; ?></p>
+                            <b class="btn btn-primary" >Inquire</b>
+                        </div>
+                    </div>
 
-    
-        <div class="productContainer">
-
-            <!-- 1 -->
-            <div class="card d-flex justify-content-center align-items-center" style="width: 22rem;">
-                <img src="public/images/products/LuxuryHome.png" alt="house with balcony">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
-            </div>
- 
-
-            <!-- 2 --> 
-            <div class="card d-flex justify-content-center align-items-center" style="width: 22rem;">
-                <img src="public/images/products/LuxuryHome.png" alt="house with balcony">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-
-            <!-- 3 --> 
-             <div class="card d-flex justify-content-center align-items-center" style="width: 22rem;">
-                <img src="public/images/products/LuxuryHome.png" alt="house with balcony">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-     
-        </div>         
+            <?php endforeach; ?>
+        </div>
+    </div>
+<!-- Parent End Tag -->
 </div>
