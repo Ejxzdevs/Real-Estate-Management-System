13928237f1c6721da7cce984c8318826c7b3e7c8
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-11-23 00:33:06 +0800

    added modal for inquiry form

--- app/view/viewProduct.php
+++ app/view/viewProduct.php
@@ -17,8 +17,6 @@ if ($encodedId) {
 } else {
     echo "No ID provided in the URL.";
 }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -26,17 +24,18 @@ if ($encodedId) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $details[0]['name']; ?></title>
+    <title><?php echo htmlspecialchars($details[0]['name']); ?></title>
     <link rel="stylesheet" href="../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
     <style>
-        html , body {
+        html, body {
             padding: 0;
             margin: 0;
             box-sizing: border-box;
             background-color: #ffffff;
         }
-       .custom-card  {
+
+        .custom-card {
             padding: 20px 10px;
             width: 600px;
             display: flex;
@@ -46,42 +45,39 @@ if ($encodedId) {
             background-color: #ffffff;
         }
 
-        .custom-card .card-header{
+        .custom-card .card-header {
             height: 2rem;
             display: flex;
             justify-content: space-between;
             align-items: center;
         }
 
-        .custom-card .card-header p{
+        .custom-card .card-header p {
             font-size: 12px;
         }
 
         .custom-card .card-header .status {
             width: auto; 
-            height: 1.3rem ;
+            height: 1.3rem;
             display: flex;
             justify-content: center;
             align-items: center;
             border-radius: 5px;
             padding: 7px !important;
             text-transform: uppercase;
-
         }
-        
 
-        .custom-card  img {
+        .custom-card img {
             height: 300px;
         }
-       
     </style>
 </head>
 <body>
     <div class="container-fluid h-100 d-flex justify-content-center align-items-center">
     <?php foreach($details as $detail): ?>
-        <div class="card custom-card border h-auto shadow"  >
+        <div class="card custom-card border h-auto shadow">
             <div class="card-header px-2 m-0">
-                <p class="fw-medium p-0 m-0"  >Date Posted: <span class="text-secondary" style="font-size: 10px;" ><?php echo htmlspecialchars($detail['date_added']); ?></span></p>
+                <p class="fw-medium p-0 m-0">Date Posted: <span class="text-secondary" style="font-size: 10px;"><?php echo htmlspecialchars($detail['date_added']); ?></span></p>
                 <p class="status p-0 m-0 text-white fw-bold 
                 <?php 
                     if ($detail['status'] == "sold") {
@@ -92,37 +88,75 @@ if ($encodedId) {
                 ?>
                     "><?php echo htmlspecialchars($detail['status']); ?></p>
             </div>
-            <img class="" src="../../public/images/products/<?php echo htmlspecialchars(string: $detail['image']); ?>" alt="<?php echo htmlspecialchars($detail['name']); ?>">
-            <div class="d-flex flex-column m-0 p-0" style="height: auto;" >
+            <img class="" src="../../public/images/products/<?php echo htmlspecialchars($detail['image']); ?>" alt="<?php echo htmlspecialchars($detail['name']); ?>">
+            <div class="d-flex flex-column m-0 p-0" style="height: auto;">
                 <p class="m-0 fw-bold fs-6"><?php echo htmlspecialchars($detail['name']); ?></p>
-                <p class="m-0 text-secondary" style="font-size: 10px;" ><i class="bi bi-geo-alt"></i><?php echo htmlspecialchars($detail['address']); ?></p>
+                <p class="m-0 text-secondary" style="font-size: 10px;"><i class="bi bi-geo-alt"></i><?php echo htmlspecialchars($detail['address']); ?></p>
             </div>
-            <div class="ps-1 pe-2  py-0" style="height: 59%;">
-                <p class="mt-4 mx-0 text-wrap" style="font-size: 12px;" ><?php echo htmlspecialchars($detail['description']); ?></p>
+            <div class="ps-1 pe-2 py-0" style="height: 59%;">
+                <p class="mt-4 mx-0 text-wrap" style="font-size: 12px;"><?php echo htmlspecialchars($detail['description']); ?></p>
             </div>
-            <div class="ps-1 pe-2 d-flex flex-row justify-content-between align-items-center" style="height: 18%;" >
-                <p class="m-0 text-secondary" >₱<?php echo htmlspecialchars($detail['price']); ?></p>
-                <?php 
-                    if ($detail['status'] == "sold") {
-                        echo '
-                        <a class="btn btn-secondary" style="font-size: 10px;">
-                            Inquires
-                        </a>';
-                    } else {
-                         echo ' 
-                         <a class="btn btn-primary" style="font-size: 10px;">
-                            Inquires
-                        </a>';
-                    }
-                ?>
-               
+            <div class="ps-1 pe-2 d-flex flex-row justify-content-between align-items-center" style="height: 18%;">
+                <p class="m-0 text-secondary">₱<?php echo htmlspecialchars($detail['price']); ?></p>
+                <button  onclick="getName('<?php echo htmlspecialchars($detail['name']); ?>')" 
+                    class=" btn <?php echo ($detail['status'] == "sold") ? 'btn-secondary' : 'btn-primary'; ?> " 
+                    style="font-size: 10px;" 
+                    data-bs-toggle="modal" 
+                    data-bs-target="#addProperty"
+                    <?php echo ($detail['status'] == "sold") ? 'disabled' : ''; ?>
+                >
+                    Inquire
+                </button>
             </div>
-            
-    
+        </div>
+    <?php endforeach; ?>
+    </div>
+
+    <!-- Add Property Modal -->
+    <div class="modal fade" id="addProperty" tabindex="-1" aria-labelledby="addProperty" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" style="font-size: 16px; ">INQUIRY FORM [<span id="houseName" class="text-secondary" style="font-size: 12px;" ></span>]</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="app/http/controller/productController.php" method="POST" id="addPropertyForm" enctype="multipart/form-data">
+                        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
+                        <input type="hidden" name="insert">
+                        <div class="mb-3">
+                            <label for="propertyName" class="form-label">Full Name</label>
+                            <input type="text" name="propertyName" class="form-control" id="propertyName" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="propertyAddress" class="form-label">Email Address</label>
+                            <input type="text" name="propertyAddress" class="form-control" id="propertyAddress" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="propertyPrice" class="form-label">Phone Number</label>
+                            <input type="text" name="propertyPrice" class="form-control" id="propertyPrice" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="propertyDescription" class="form-label">Message<span class="text-secondary">(Optional)</span></label>
+                            <textarea name="propertyDescription" class="form-control" id="propertyDescription" rows="3"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" form="addPropertyForm">Submit</button>
+                </div>
+            </div>
         </div>
     </div>
-    <?php endforeach; ?>
+
+    <script>
+        function getName(name) {
+            houseName = document.getElementById('houseName').textContent = name;
+            
+        }
+    </script>
     
-</body>
     <script src="../../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
