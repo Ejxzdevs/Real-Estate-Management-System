663a6da21a9d6be7ac11b5fb64fdea9d81c692de
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-11-23 14:16:12 +0800

    change query to select all non deleted product

--- app/http/helper/displayProduct.php
+++ app/http/helper/displayProduct.php
@@ -5,7 +5,7 @@ class DisplayProduct extends MySQL {
 
     public function getAllProducts() {
         try {
-            $stmt = parent::openConnection()->prepare("SELECT * FROM properties");
+            $stmt = parent::openConnection()->prepare("SELECT * FROM properties where is_deleted = 0");
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
@@ -18,7 +18,7 @@ class DisplayProduct extends MySQL {
 
     public function recentProducts($limit) {
         try {
-            $stmt = parent::openConnection()->prepare("SELECT * FROM properties ORDER BY date_added DESC LIMIT :limit");
+            $stmt = parent::openConnection()->prepare("SELECT * FROM properties where is_deleted = 0 ORDER BY date_added DESC LIMIT :limit");
             $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
