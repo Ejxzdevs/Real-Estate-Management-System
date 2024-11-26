db1d23ab6119d9bb337a57d39c01c43e14cd9ec9
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-11-26 13:49:50 +0800

    adding alias for address

---
+++ app/http/helper/displayInventory.php
@@ -0,0 +1,25 @@
+<?php 
+require_once 'config/mysql.php';
+
+class DisplayInventory extends MySQL {
+
+    public function getInventory() {
+        try {
+            $stmt = parent::openConnection()->prepare("
+            SELECT inventory.*, 
+            Properties.*, 
+            Properties.address AS property_address, 
+            inventory.address AS inventory_address
+            FROM inventory
+            JOIN Properties ON inventory.property_id = Properties.id;; 
+            ");
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        } finally {
+            parent::closeConnection();
+        }
+    }
+}
