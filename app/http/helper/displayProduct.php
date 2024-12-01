587d7a31596216fc27a58fb030a1c71971483754
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-01 16:28:37 +0800

    added method for total rent and sell

--- app/http/helper/displayProduct.php
+++ app/http/helper/displayProduct.php
@@ -29,4 +29,28 @@ class DisplayProduct extends MySQL {
             parent::closeConnection();
         }
     }
+
+    // Number of Rent
+    public function NumRentSell(){
+
+        try {
+            $stmt = parent::openConnection()->prepare("
+            SELECT 
+                SUM(CASE WHEN transaction_type = 'rent' THEN 1 ELSE 0 END) AS Total_Rent,
+                SUM(CASE WHEN transaction_type = 'sell' THEN 1 ELSE 0 END) AS Total_Sell
+            FROM properties WHERE is_deleted = 0
+            ");
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        } finally {
+            parent::closeConnection();
+        }
+
+    }
+
+
+   
 }
