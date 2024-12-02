7bbf0cbe6a4bf4dd39133bc8dcac7a09ed7d9e81
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-03 00:29:18 +0800

    add preview Image for update

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -20,6 +20,27 @@ function previewImage(event) {
     }
 }
 
+// 
+
+function previewUpdateImage(event) {
+    const imagePreview = document.getElementById('imageUpdatePreview');
+    const file = event.target.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+        
+        reader.onload = function(e) {
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = 'block';
+        }
+        
+        reader.readAsDataURL(file);
+    } else {
+        imagePreview.src = '';
+        imagePreview.style.display = 'none';
+    }
+}
+
 // Inventory
 
 // For Add Modal
