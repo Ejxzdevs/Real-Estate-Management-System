589a4f003cc28ca0cef32c673b5b047b556dfddc
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-03 14:10:35 +0800

    init

---
+++ app/view/about.php
@@ -0,0 +1,22 @@
+<div class="p-5" style="height: 100vh;">
+
+    <div class="d-flex justify-content-center align-items-center h-25" >
+        <p class="fs-1 mt-5 fw-bold" >OUR COMPANY</p>
+    </div>
+    <div class="container h-75 d-flex" >
+        <div class="row w-100">
+            <div class="col-6">
+                <img class="h-100 rounded-3" src="public/images/Backgrounds/about.jpg" alt="">
+            </div>
+            <div class="col-6 d-flex flex-column justify-content-center gap-2 px-4">
+                <div>
+                    <label class="fw-medium fs-3">About Us</label>
+                </div>
+                <div>
+                    <p>At Real Ease, we are dedicated to simplifying and streamlining real estate management. Our cutting-edge platform provides a comprehensive solution for property owners, managers, and tenants, making it easier to manage, monitor, and maintain properties. With Real Ease, you can efficiently handle leasing, payments, maintenance requests, and more—all in one place. Whether you're a property manager or a tenant, our goal is to make your experience as smooth and hassle-free as possible. Trust Real Ease to bring convenience, transparency, and control to the world of real estate management.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
